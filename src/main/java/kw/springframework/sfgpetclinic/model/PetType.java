40244ccb08c2a17c50b6b53b90913125365ae2d0
keith <keithw453@gmail.com>
2018-10-05 22:24:27 -0400

    adding POJOS. Closes #2

---
+++ src/main/java/kw/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package kw.springframework.sfgpetclinic.model;
+
+class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/kw/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package kw.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
