a05e44dd5922d9ae2b0d5371297be68fe512612a
keith <keithw453@gmail.com>
2018-10-13 21:53:42 -0400

    Adding bootstrap for data loading. CLoses #14

--- pet-clinic-data/src/main/java/kw/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/kw/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,10 +2,11 @@ package kw.springframework.sfgpetclinic.services.map;
 
 import kw.springframework.sfgpetclinic.model.Vet;
 import kw.springframework.sfgpetclinic.services.CrudService;
+import kw.springframework.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long>  implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long>  implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
