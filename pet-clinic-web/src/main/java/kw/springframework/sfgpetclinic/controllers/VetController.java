a65e9013c713a4de4344ac45a9e8c6869186ac4a
keith <keithw453@gmail.com>
2018-10-14 09:48:16 -0400

    Added index page for vets. closes #15

--- pet-clinic-web/src/main/java/kw/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/kw/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package kw.springframework.sfgpetclinic.controllers;
 
+import kw.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"", "/", "index", "index.html"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
