cb4ceee2c4978dc3379a6e6647d0c84504dba3d5
keith <keithw453@gmail.com>
2018-10-21 15:01:15 -0400

    added PetTypeService. Closes #28

--- pet-clinic-data/src/main/java/kw/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/kw/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package kw.springframework.sfgpetclinic.model;
 
-class PetType extends BaseEntity{
+public class PetType extends BaseEntity{
 
     private String name;
 
---
+++ pet-clinic-data/src/main/java/kw/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package kw.springframework.sfgpetclinic.services;
+import kw.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by keith on 10/21/18.
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
